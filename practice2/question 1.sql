16f28aff56a037e34733c0b4a241e1d778281c15
Kartik Arora <kartikar@uw.edu>
2018-06-05 13:40:53 -0700

    part 2

--- practice2/question 1.sql
+++ practice2/question 1.sql
@@ -9,7 +9,7 @@
 -- Part 1 for each bar, count the number of drinkers that frequent it, and that like both 'heineken' and 'bud'
 --
 
-Select F.bar, count(drinker)
+Select F.bar, count(distinct F.drinker)
 From Frequents as F, Likes as L, Likes as L2
 where L.drinker = F.drinker
   And L2.drinker = F.drinker
@@ -18,4 +18,12 @@ where L.drinker = F.drinker
 Group by F.bar;
 -- * for each bar that serve both 'heineken' and 'bud' count the number of drinkers that frequent that bar
 --
+
+Select F.bar, count(distinct F.drinker)
+From Serves as s1, Serves as s2, Frequents as F
+Where s1.bar = F.bar
+  And s2.bar = F.bar
+  And S1.beer = 'heineken'
+  And S2.beer = 'bud';
+Group by F.bar;
 -- * for each bar that serve 'heineken' count the number of drinkers that frequent that bar and that like 'bud'
